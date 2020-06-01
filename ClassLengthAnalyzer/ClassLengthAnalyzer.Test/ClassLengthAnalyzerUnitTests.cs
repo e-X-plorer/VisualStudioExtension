f59cfe96e5b4621e83829eef78f4d66c5176b018
Kitsune <44873289+KitsuNeko256@users.noreply.github.com>
2020-06-01 22:04:52 +0300

    Adapted previous tests to changes

--- ClassLengthAnalyzer/ClassLengthAnalyzer.Test/ClassLengthAnalyzerUnitTests.cs
+++ ClassLengthAnalyzer/ClassLengthAnalyzer.Test/ClassLengthAnalyzerUnitTests.cs
@@ -13,15 +13,16 @@ namespace ClassLengthAnalyzer.Test
 
         //No diagnostics expected to show up
         [TestMethod]
-        public void TestMethod1()
+        public void ZeroInputTest()
         {
             var test = @"";
 
             VerifyCSharpDiagnostic(test);
         }
-
+        
+        //No Diagnostics expected to show up
         [TestMethod]
-        public void TestTest()
+        public void EmptyClassTest()
         {
             var test = @"
     using System;
@@ -34,13 +35,206 @@ namespace ClassLengthAnalyzer.Test
     namespace ConsoleApplication1
     {
         class TypeName
+        {   
+        }
+    }";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //No Diagnostics expected to show up
+        [TestMethod]
+        public void SmallClassTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {   
+            public const int var1 = 1;
+            public const int var2 = 2;
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //Diagnostic triggered on too many members
+        [TestMethod]
+        public void ManyMembersTest()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            public const int var4 = 4;
+            public const int var5 = 5;
+            public const int var6 = 6;
+            public const int var7 = 7;
+            public const int var8 = 8;
+            public const int var9 = 9;
+            public const int var10 = 10;
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "ClassTooLong",
+                Message = String.Format("Class {0} contains {1}. Maximum allowed is {2}. Class can be divided.", "TypeName", "11 members", "5"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 4, 15)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+        }
+
+        //Diagnostic triggered on too many lines
+        [TestMethod]
+        public void ManyLinesTest()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "ClassTooLong",
+                Message = String.Format("Class {0} contains {1}. Maximum allowed is {2}. Class can be divided.", "TypeName", "25 lines", "15"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 4, 15)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+        }
+
+        //Diagnostic triggered on too many members
+        [TestMethod]
+        public void ManyMembersManyLinesTest()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            public const int var4 = 4;
+            public const int var5 = 5;
+            public const int var6 = 6;
+            public const int var7 = 7;
+            public const int var8 = 8;
+            public const int var9 = 9;
+            public const int var10 = 10;
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "ClassTooLong",
+                Message = String.Format("Class {0} contains {1}. Maximum allowed is {2}. Class can be divided.", "TypeName", "11 members", "5"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 4, 15)
+                        }
+            };
+
+            VerifyCSharpDiagnostic(test, expected);
+
+        }
+
+        [TestMethod]
+        public void TestTest()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
         {
             public int x1;
             public int x2;
             public int x3;
             public int f1()
             {
-            
+
 
 
 
@@ -57,54 +251,6 @@ namespace ClassLengthAnalyzer.Test
         }
     }";
             VerifyCSharpFix(test, test);
-        }
-
-        //Diagnostic and CodeFix both triggered and checked for
-        [TestMethod]
-        public void TestMethod2()
-        {
-            var test = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TypeName
-        {   
-        }
-    }";
-            var expected = new DiagnosticResult
-            {
-                Id = "ClassLengthAnalyzer",
-                Message = String.Format("Type name '{0}' contains lowercase letters", "TypeName"),
-                Severity = DiagnosticSeverity.Warning,
-                Locations =
-                    new[] {
-                            new DiagnosticResultLocation("Test0.cs", 11, 15)
-                        }
-            };
-
-            VerifyCSharpDiagnostic(test, expected);
-
-            var fixtest = @"
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-    using System.Diagnostics;
-
-    namespace ConsoleApplication1
-    {
-        class TYPENAME
-        {   
-        }
-    }";
-            VerifyCSharpFix(test, fixtest);
         }
 
         protected override CodeFixProvider GetCSharpCodeFixProvider()
