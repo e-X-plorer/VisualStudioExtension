302deef0309af23b4a39c53930a26e6d5e473407
Ilya Zinovev <zmea07@gmail.com>
2020-06-06 00:22:49 +0300

    Added tests for two classes

--- ClassLengthAnalyzer/ClassLengthAnalyzer.Test/ClassLengthAnalyzerUnitTests.cs
+++ ClassLengthAnalyzer/ClassLengthAnalyzer.Test/ClassLengthAnalyzerUnitTests.cs
@@ -232,6 +232,47 @@ namespace ClassLengthAnalyzer.Test
             public int x1;
             public int x2;
             public int x3;
+            public int x4;
+            public int x5;
+            public int x6;
+            public int x7;
+        }
+    }";
+            var testUpdated = @"
+    namespace ConsoleApplication1
+    {
+    partial class TypeName
+        {
+            public int x1;
+            public int x2;
+            public int x3;
+            public int x4;
+            public int x5;
+        }
+    }";
+            var testCreated = @"namespace ConsoleApplication1
+{
+    partial class TypeName
+        {
+            public int x6;
+            public int x7;
+        }
+}";
+            VerifyCSharpCreatedFix(test, testUpdated, testCreated);
+        }
+
+        //Diagnostic triggered on too many lines, fix applied, updated and created files checked
+        [TestMethod]
+        public void ManyLinesFixTest()
+        {
+            var test = @"
+    namespace ConsoleApplication1
+    {
+        class TypeName
+        {
+            public int x1;
+            public int x2;
+            public int x3;
             public int f1()
             {
 
@@ -284,6 +325,259 @@ namespace ClassLengthAnalyzer.Test
 }";
             VerifyCSharpCreatedFix(test, testUpdated, testCreated);
         }
+
+        //No Diagnostics expected to show up
+        [TestMethod]
+        public void TwoSmallClassesTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Class1
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+        
+        class Class2
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            VerifyCSharpDiagnostic(test);
+        }
+
+        //Diagnostic triggered on too many lines in 2nd class
+        [TestMethod]
+        public void SmallAndManyLinesTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Class1
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+        
+        class Class2
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "ClassTooLong",
+                Message = String.Format("Class {0} contains {1}. Maximum allowed is {2}. Class can be divided.", "Class2", "20 lines", "15"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 27, 15)
+                        }
+            };
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        //Diagnostic triggered on too many members in 1st class 
+        [TestMethod]
+        public void SmallAndManyMembersTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Class1
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            public const int var4 = 4;
+            public const int var5 = 5;
+
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+        
+        class Class2
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            var expected = new DiagnosticResult
+            {
+                Id = "ClassTooLong",
+                Message = String.Format("Class {0} contains {1}. Maximum allowed is {2}. Class can be divided.", "Class1", "6 members", "5"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 11, 15)
+                        }
+            };
+            VerifyCSharpDiagnostic(test, expected);
+        }
+
+        //Diagnostic triggered on many members in 1st class and many lines in 2nd class
+        [TestMethod]
+        public void ManyMembersAndManyLinesTest()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Class1
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            public const int var4 = 4;
+            public const int var5 = 5;
+
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+        
+        class Class2
+        {
+            public const int var1 = 1;
+            public const int var2 = 2;
+            public const int var3 = 3;
+            
+            private Command1(AsyncPackage package, OleMenuCommandService commandService)
+            {
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+
+                this.package = package ?? throw new ArgumentNullException(nameof(package));
+                commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+                var menuCommandID = new CommandID(CommandSet, CommandId);
+                var menuItem = new MenuCommand(this.Execute, menuCommandID);
+                commandService.AddCommand(menuItem);
+            }
+        }
+    }";
+            var expectedManyMembers = new DiagnosticResult
+            {
+                Id = "ClassTooLong",
+                Message = String.Format("Class {0} contains {1}. Maximum allowed is {2}. Class can be divided.", "Class1", "6 members", "5"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[] {
+                            new DiagnosticResultLocation("Test0.cs", 11, 15)
+                        }
+            };
+            var expectedManyLines = new DiagnosticResult
+            {
+                Id = "ClassTooLong",
+                Message = String.Format("Class {0} contains {1}. Maximum allowed is {2}. Class can be divided.", "Class2", "20 lines", "15"),
+                Severity = DiagnosticSeverity.Warning,
+                Locations =
+                    new[]
+                    {
+                        new DiagnosticResultLocation("Test0.cs", 29, 15)
+                    }
+            };
+            VerifyCSharpDiagnostic(test, expectedManyMembers, expectedManyLines);
+        }     
 
         protected override CodeFixProvider GetCSharpCodeFixProvider()
         {
